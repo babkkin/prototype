ced9baf4d56c8b05cb52b574dedbd2bd990ee656
pafmarasigan@gmail.com <pafmarasigan@gmail.com>
2026-09-10 14:56:27 +0800

    Add title to Medicines screen app bar

--- lib/screens/medicine.dart
+++ lib/screens/medicine.dart
@@ -7,6 +7,7 @@ class MedicineScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text('Medicines'),
       ),
       body: const Center(
         child: Text('Medicines Content'),
